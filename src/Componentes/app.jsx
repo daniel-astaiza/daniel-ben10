f1542be969707fa137dfe4081da567aebad10069
daniel-astaiza <danielastaiza111@gmail.com>
2023-11-22 09:11:29 -0500

    se agrego un componente

---
+++ src/Componentes/app.jsx
@@ -0,0 +1,20 @@
+const app = ({img, precio, descripcion}) => {
+    return (
+        <div>
+            <div className="">
+                <div className="card ">
+                    <img src={img} className="" />
+                    <div className=" d-flex justify-content-center card-body">
+                        <p className="card-text text-center"><b>{descripcion}</b></p>
+                    </div>
+                    <div className="card-footer d-flex justify-content-center">
+                        <small className="card-text">$ {precio}</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default app;
